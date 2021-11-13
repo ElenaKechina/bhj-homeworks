04067766f64cad9e2febd21bdeb517e5102cb5d4
Elena Kechina <elenakechina86@gmail.com>
2021-11-13 12:55:24 +0300

    home_Work#6_2

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,12 +1,20 @@
 const click = (event) => {
   const target = event.target.closest('.has-tooltip');
+
   if (!target) {
     return;
   }
-  const removeTooltip = document.querySelector('.tooltip');
-  removeTooltip.remove();
-
   event.preventDefault();
+
+  const removeTooltip = document.querySelector('.tooltip');
+
+  if (removeTooltip) {
+    removeTooltip.remove();
+
+    if (target.title === removeTooltip.textContent) {
+      return;
+    }
+  }
 
   const tooltip = document.createElement('div');
   tooltip.textContent = target.title;
